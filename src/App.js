a8d28da98eacf40302b7d4648358c416b40bac26
Medha Dhir <medha499@dhcp-wifi-8021x-168-122-219-77.bu.edu>
2021-09-04 18:38:25 -0400

    getting error

--- src/App.js
+++ src/App.js
@@ -10,6 +10,39 @@ import Index from './components/Index';
 
 import "./css/main.css";
 
+import './../../public/images/about-event-splash.jpg';
+import './../../public/images/about-hero.png';
+import './../../public/images/blueline.png';
+import './../../public/images/bluetri.png';
+
+import './../../public/images/aBu-logo.png';
+import './../../public/images/clubs-landing.png';
+import './../../public/images/greencirclehollow.png';
+import './../../public/images/greenline.png';
+import './../../public/images/greentriangle.png';
+import './../../public/images/home_hero.png';
+import './../../public/images/home_mission.png';
+import './../../public/images/home_testimonial1.png';
+import './../../public/images/home_testimonial2.png';
+import './../../public/images/home_testimonial3.png';
+
+import './../../public/images/largegreencircle.png';
+import './../../public/images/largeredhex.png';
+import './../../public/images/mailing-kist-background.png';
+import './../../public/images/purplecircle.png';
+import './../../public/images/purpleline.png';
+import './../../public/images/redsmallhex.png';
+import './../../public/images/reg logo no background.png';
+
+import './../../public/images/stock-img-man-1.png';
+import './../../public/images/stock-img-man-2.png';
+import './../../public/images/stock-img-woman-1.png';
+import './../../public/images/stock-img-woman-2.png';
+
+import './../../public/images/tinygreencircle.png';
+import  './../../public/images/yellowemptysqr.png';
+import  './../../public/images/yellowline.png';
+
 function App() {
   return (
     <main className="CSA Official Website">
